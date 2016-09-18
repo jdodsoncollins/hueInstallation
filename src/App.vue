f2067188ece1c45aac7e0f22eac8660bf2883683
Jason Raimondi <jason@raimondi.us>
2016-09-18 16:06:25 -0700

    Cleanup.

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,13 @@
     height: 100%;
     width: 100%;
   }
-
   body {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
     background: url('./assets/images/gifblur.gif') no-repeat;
     background-size: 100% 100%;
-    position: fixed;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    right: 0;
     z-index: -999;
   }
-
   .main-container {
-    margin-bottom: 100px;
   }
-
   .app {
     background: url('./assets/images/gradientbg.png') no-repeat;
     background-size: 100% 100%;
--- src/components/SwitchBoard.vue
+++ src/components/SwitchBoard.vue
@@ -1,109 +1,64 @@
 <template>
-  <div class="owl-carousel owl-theme">
-      <heart :icon="icons.heartIcon" @click="submitVote(0)"></heart>
-      <heart :icon="icons.peachIcon" @click="submitVote(1)"></heart>
-      <heart :icon="icons.sunIcon" @click="submitVote(2)"></heart>
-      <heart :icon="icons.smileIcon" @click="submitVote(3)"></heart>
-      <heart :icon="icons.voltIcon" @click="submitVote(4)"></heart>
-      <heart :icon="icons.waterIcon" @click="submitVote(5)"></heart>
-      <heart :icon="icons.musicIcon" @click="submitVote(6)"></heart>
-      <heart :icon="icons.flowerIcon" @click="submitVote(7)"></heart>
-  </div>
+  <section class="owl-carousel owl-theme">
+    <switch :icon="icons.heartIcon" @click="submitVote(0)"></switch>
+    <switch :icon="icons.peachIcon" @click="submitVote(1)"></switch>
+    <switch :icon="icons.sunIcon" @click="submitVote(2)"></switch>
+    <switch :icon="icons.smileIcon" @click="submitVote(3)"></switch>
+    <switch :icon="icons.voltIcon" @click="submitVote(4)"></switch>
+    <switch :icon="icons.waterIcon" @click="submitVote(5)"></switch>
+    <switch :icon="icons.musicIcon" @click="submitVote(6)"></switch>
+    <switch :icon="icons.flowerIcon" @click="submitVote(7)"></switch>
+  </section>
 </template>
-
-<style>
-  .vote-icon {
-    z-index: 999;
-  }
-</style>
 
 <script>
 
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import $ from 'jquery';
-import 'imports?jQuery=jquery!owl.carousel';
+  import heartIcon from '../assets/images/icons/heart.png';
+  import peachIcon from '../assets/images/icons/peach.png';
+  import sunIcon from '../assets/images/icons/sun.png';
+  import smileIcon from '../assets/images/icons/smile.png';
+  import voltIcon from '../assets/images/icons/volt.png';
+  import waterIcon from '../assets/images/icons/water.png';
+  import musicIcon from '../assets/images/icons/music.png';
+  import flowerIcon from '../assets/images/icons/flower.png';
 
-import Switch from './Switch';
-import Firebase from 'firebase';
-import Heart from './Heart';
+  import 'owl.carousel/dist/assets/owl.carousel.css';
+  import $ from 'jquery';
+  import 'imports?jQuery=jquery!owl.carousel';
 
-import heartIcon from '../assets/images/icons/heart.png';
-import peachIcon from '../assets/images/icons/peach.png';
-import sunIcon from '../assets/images/icons/sun.png';
-import smileIcon from '../assets/images/icons/smile.png';
-import voltIcon from '../assets/images/icons/volt.png';
-import waterIcon from '../assets/images/icons/water.png';
-import musicIcon from '../assets/images/icons/music.png';
-import flowerIcon from '../assets/images/icons/flower.png';
+  import Switch from './Switch';
 
-export default {
-  name: 'SwitchBoard',
-
-  components: {
-    Switch, Heart
-  },
-
-  data() {
-    return {
-      loggedData: {},
-      url: 'http://10.14.1.146/api/vfcRmQVlEUHHsVzyFHkEGTI4DqZVwbzUQzoxYR3X/lights',
-      lightResource: null,
-      selectedIndex: 0,
-      selectedName: '',
-      icons: {
-        heartIcon: heartIcon,
-        peachIcon: peachIcon,
-        sunIcon: sunIcon,
-        smileIcon: smileIcon,
-        voltIcon: voltIcon,
-        waterIcon: waterIcon,
-        musicIcon: musicIcon,
-        flowerIcon: flowerIcon,
-      }
-    };
-  },
-
-  ready() {
-    $('.owl-carousel').owlCarousel({
-      loop: true,
-//      margin: 30,
-      slideBy: 100,
-      dots: false
-    });
-  },
-
-  methods: {
-    submitVote(id) {
-      console.log(id);
-    }
-  },
-
-  events: {
-    switchIndex(msg) {
-
-      // this is a property sent up from the child
-      this.selectedIndex = msg;
-      const colorOption = Firebase.database().ref("Lights/option" + this.selectedIndex);
-
-      colorOption.transaction(function(post) {
-        if (post == null) {
-          return 0;
+  export default {
+    name: 'SwitchBoard',
+    components: {
+      Heart: Heart
+    },
+    data() {
+      return {
+        icons: {
+          heartIcon: heartIcon,
+          peachIcon: peachIcon,
+          sunIcon: sunIcon,
+          smileIcon: smileIcon,
+          voltIcon: voltIcon,
+          waterIcon: waterIcon,
+          musicIcon: musicIcon,
+          flowerIcon: flowerIcon,
         }
-
-        return ++post;
-      }).then(function (stuff) {
-        console.log('stuff', stuff);
-      }).catch(function (error) {
-        console.log('error', error);
+      };
+    },
+    ready() {
+      $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        slideBy: 100,
+        dots: false
       });
     },
-    switchName(msg) {
-      // this is a property sent up from the child
-      this.selectedName = msg;
-      // console.log(msg);
+    methods: {
+      submitVote(id) {
+        console.log('cast vote for: ' + id);
+      }
     },
-  },
-
-};
-
+  };
 </script>
